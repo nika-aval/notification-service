db01c5e264efb299805569ecac1c7b571d4f46bc
Nika Avalishvili <nika.avalishvili@ug.edu.ge>
2024-11-20 15:43:04 +0400

    Fixes existing issues

--- src/main/java/pdp/notification_service/dto/UtilityBillDto.java
+++ src/main/java/pdp/notification_service/dto/UtilityBillDto.java
@@ -1,8 +1,22 @@
 package pdp.notification_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record UtilityBillDto(Long Id, LocalDateTime date, BigDecimal Amount, boolean isPaid,
-                             SubscriptionProviderDto subscriptionProvider, CustomerDto customer) {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UtilityBillDto implements Serializable {
+    private Long id;
+    private LocalDateTime date;
+    private BigDecimal amount;
+    private boolean isPaid;
+    private SubscriptionProviderDto subscriptionProvider;
+    private CustomerDto customer;
 }
+
